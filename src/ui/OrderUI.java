6dbba96e8919f46b97580b51459dfad29dc91898
dxwnight <ilovedawn97@gmail.com>
2019-03-27 01:24:42 +0800

    Changed OrderUI methods

--- src/ui/OrderUI.java
+++ src/ui/OrderUI.java
@@ -33,11 +33,11 @@ public class OrderUI {
 			System.out.println("\n================================");
 			System.out.println("=== [Orders] ===");
 			System.out.println("Choose an option:");
-			System.out.println("[1] View all Orders");
+			System.out.println("[1] View all Orders"); //maybe can do for current/completed orders?
 			System.out.println("[2] Create Order");
 			System.out.println("[3] Edit Order");
 			System.out.println("[4] Remove Order");
-			System.out.println("[5] Print Invoice");
+			//System.out.println("[5] Print Invoice");
 			System.out.println("[6] Return to Main Menu");
 			System.out.println("[0] Exit RRPSS");
 			System.out.println("================================");
@@ -52,12 +52,15 @@ public class OrderUI {
 					createOrder();
 					break;
 				case 3: //Edit an order
+					editOrder();
 					break;
 				case 4: //Remove an order
+					removeOrder();
 					break;
 				case 5: //Print invoice
 					break;
 				case 6: //Return to Main Menu
+					RRPSSApp.showMainMenu();
 					break;
 					
 				default: 
@@ -75,10 +78,24 @@ public class OrderUI {
 		
 		if(Restaurant.orders.size() > 0)
 		{
+			//need change the ui
 			for(int i = 0; i < Restaurant.orders.size();i++)
 			{
-				System.out.println("("+ Restaurant.orders.get(i).getId() + ") " + Restaurant.orders.get(i).getItems() + " | Table No." 
-									+ Restaurant.orders.get(i).getTable().getId() + " | " + Restaurant.orders.get(i).getOrderDateTime());
+				System.out.println("(Order No: "+ Restaurant.orders.get(i).getId() + ")");
+				
+				for(int j = 0; j < Restaurant.orders.get(i).getItems().size(); j++)
+				{
+					System.out.println(Restaurant.orders.get(i).getItems().get(j).getName());
+					
+					if(j+1 !=  Restaurant.orders.get(i).getItems().size())
+					{
+						System.out.print(", ");
+					}
+							
+				}
+				
+				System.out.println("\n(Table No: " + Restaurant.orders.get(i).getTable().getId() + ")\n(" + Restaurant.orders.get(i).getOrderDateTime() + ")");
+				System.out.println("Created by " + Restaurant.orders.get(i).getStaff().getName());
 			}
 		}
 		else
@@ -93,7 +110,7 @@ public class OrderUI {
 		
 		ArrayList<SaleItem> saleItems = new ArrayList<SaleItem>();
 		Order order;
-		int choice = 0, orderSize = 0;
+		int choice = 0, orderSize = 0, quantity = 0;
 		char option;
 		
 		System.out.println("\n[Menu]\n");
@@ -104,8 +121,6 @@ public class OrderUI {
 								+ Restaurant.menuItems.get(i).getPrice() + " | " + Restaurant.menuItems.get(i).getMenuType());
 		}
 		
-		
-		
 		do{
 			System.out.println("Add food to order: ");
 			choice = sc.nextInt();
@@ -114,14 +129,7 @@ public class OrderUI {
 			{
 				if(Restaurant.menuItems.get(i).getId() == choice)
 				{	
-					if(orderSize == 0)
-					{
-						saleItems.add(Restaurant.menuItems.get(i));
-					}
-					else
-					{
-						System.out.println("Menu item already been added.");
-					}
+					saleItems.add(Restaurant.menuItems.get(i));
 					
 					break;
 				}
@@ -134,8 +142,18 @@ public class OrderUI {
 				
 			}
 			
+			/*
+			System.out.println("Quantity of " + saleItems.get(orderSize).getName() + ": ");
+			quantity = sc.nextInt();
+			
+			for(int i = 0; i < quantity; i++)
+			{
+				saleItems.add(Restaurant.menuItems.get(i));
+			}*/
+			
+			
 			System.out.print("Continue adding to order? (Y/N): ");
-			option = Character.toUpperCase(sc.next().charAt(0));
+			option = Character.toUpperCase(sc.next().charAt(0)); //check if user input is char
 			
 		}while(option=='Y');
 		
@@ -149,6 +167,49 @@ public class OrderUI {
 		Restaurant.orders.add(order);
 		
 		System.out.print("Order complete!");
+		
+	}
+	
+	//Add to Manager?
+	//Incomplete
+	public static void editOrder() {
+		
+	}
+	
+	//Add to Manager?
+	//Incomplete
+	public static void removeOrder() {
+		
+		int choice = 0;
+		
+		viewOrder();
+		
+		System.out.println("\n[Remove Orders]\n");
+		
+		System.out.println("Please enter the order ID to remove : ");
+		choice = sc.nextInt();
+		
+		if(Restaurant.orders.size() > 0)
+		{
+			for(int i = 0; i < Restaurant.orders.size();i++)
+			{
+				if(Restaurant.orders.get(i).getId() == choice)
+				{
+					Restaurant.orders.remove(Restaurant.orders.get(i));
+					System.out.println("Order successfully removed!");
+					break;
+				}
+				
+				if(i+1 == Restaurant.orders.size())
+				{
+					System.out.println("Order not found.");
+				}
+			}
+		}
+		else
+		{
+			System.out.println("No orders found.");
+		}
 		
 	}
 }
