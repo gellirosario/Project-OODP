b61c7d4aceafeb7c664f69bd36ab4633802aa778
wongwanting <wong1089@e.ntu.edu.sg>
2019-04-01 21:50:26 +0800

    added attributes, constructor, getters/setters

--- src/classes/Invoice.java
+++ src/classes/Invoice.java
@@ -1,5 +1,52 @@
 package classes;
 
 public class Invoice {
-
+	private static final long serialVersionUID = 4L;
+	
+	private int id;
+	
+	private Order order;
+	
+	private double tax;
+	
+	private double svcChrg;
+	
+	public Invoice(int id, Order order, double tax, double svcChrg) {
+		this.setId(id);
+		this.setOrder(order);
+		this.setTax(tax);
+		this.setSvcChrg(svcChrg);
+	}
+	
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public Order getOrder() {
+		return order;
+	}
+	
+	public void setOrder(Order order) {
+		this.order = order;
+	}
+	
+	public double getTax() {
+		return tax;
+	}
+	
+	public void setTax(double tax) {
+		this.tax = tax;
+	}
+	
+	public double getSvcChrg() {
+		return svcChrg;
+	}
+	
+	public void setSvcChrg(double svcChrg) {
+		this.svcChrg = svcChrg;
+	}
 }
