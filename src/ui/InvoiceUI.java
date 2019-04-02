dfb43289238b5399d4e00645d3f445440e461444
wongwanting <wong1089@e.ntu.edu.sg>
2019-04-02 13:26:55 +0800

    added check for valid order id

--- src/ui/InvoiceUI.java
+++ src/ui/InvoiceUI.java
@@ -9,13 +9,19 @@ public class InvoiceUI {
 	
 	public static Scanner sc = new Scanner(System.in);
 	
+	//need to add check for integer input only
 	public static void showInvoiceUI() {
 		int orderId = 0;
 		
 		System.out.println("Please enter the order ID: ");
 		orderId = sc.nextInt();
 		
-		//call printInvoice() in InvoiceManager
-		InvoiceManager.printInvoice(orderId);
+		//if it is a valid id - call printInvoice
+		if(InvoiceManager.checkId(orderId)) {
+			InvoiceManager.printInvoice(orderId);
+		}
+		else {
+			System.out.println("Order not found. Unable to print invoice.");
+		}
 	}
 }
