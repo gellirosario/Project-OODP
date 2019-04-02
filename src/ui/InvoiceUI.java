f1e236ef8a1613eed51c164aade62a272f5e73fa
wongwanting <wong1089@e.ntu.edu.sg>
2019-04-02 16:46:01 +0800

    added ArrayList<Orders> to get order list from restaurant class

--- src/ui/InvoiceUI.java
+++ src/ui/InvoiceUI.java
@@ -1,7 +1,10 @@
 package ui;
 
 import java.util.Scanner;
+import java.util.ArrayList;
 
+import classes.Order;
+import classes.Restaurant;
 import mgr.InvoiceManager;
 
 
@@ -9,19 +12,32 @@ public class InvoiceUI {
 	
 	public static Scanner sc = new Scanner(System.in);
 	
-	//need to add check for integer input only
+	private static Restaurant restaurant;
+	
 	public static void showInvoiceUI() {
+		
 		int orderId = 0;
 		
+		ArrayList<Order> orders = restaurant.getOrders();
+		
+		Order order = null;
+		
+		if(orders.size() == 0) {
+			System.out.println("No orders found. Unable to print invoice.");
+		}
+		
+		//need to add check for integer input only
 		System.out.println("Please enter the order ID: ");
 		orderId = sc.nextInt();
-		
-		//if it is a valid id - call printInvoice
-		if(InvoiceManager.checkId(orderId)) {
-			InvoiceManager.printInvoice(orderId);
-		}
-		else {
-			System.out.println("Order not found. Unable to print invoice.");
+
+		for(int i = 0; i < orders.size(); i++) {
+			if(orders.get(i).getId() == orderId) {
+				order = orders.get(i);
+				InvoiceManager.printInvoice(orderId);
+			}
+			if(i+1 == orders.size()){
+				System.out.println("Order not found. Unable to print invoice");
+			}
 		}
 	}
 }
