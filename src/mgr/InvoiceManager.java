6abbbd793918f61de665cd1a32f1b303c07a1b23
wongwanting <wong1089@e.ntu.edu.sg>
2019-04-02 19:03:12 +0800

    added removeDuplicate method

--- src/mgr/InvoiceManager.java
+++ src/mgr/InvoiceManager.java
@@ -3,7 +3,6 @@ package mgr;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-//import app.RRPSSApp;
 import classes.Order;
 import classes.Restaurant;
 import classes.Invoice;
@@ -15,18 +14,33 @@ import ui.InvoiceUI;
 
 public class InvoiceManager {
 	
+	public static final double GST = 0.07;
+	public static final double SVC_CHRG = 0.10;
+	
+	private static Restaurant restaurant;
+	
 	public static void printInvoice(Order order) {
 		
-		//retrieve order
-		for(int i = 0; i < Restaurant.orders.size(); i++) {
-			
-			if(Restaurant.orders.get(i).getId() == orderId) {
-				order = Restaurant.orders.get(i);
-			}
-		}
-		//retrieve total tax
-		//retrieve total svcChrg
-		//retrieve subTotal
+		double tax = 0.0;
+		double svcChrg = 0.0;
+		double subTotal = 0.0;
+		
+		Invoice invoice = null;
+		
+		ArrayList<Invoice> invoices = restaurant.getInvoices();
+		
+		ArrayList<SaleItem> saleItems = order.getItems();
+		
+		subTotal = calSubTotal(order);
+		
+		tax = GST * subTotal;
+		
+		svcChrg = SVC_CHRG * subTotal;
+		
+		invoice = new Invoice(invoices.size()+1, order, tax, svcChrg, subTotal);
+		
+		invoices.add(invoice);
+
 		
 	}
 	
@@ -34,13 +48,27 @@ public class InvoiceManager {
 		
 		double subTotal = 0.0;
 		
-		SaleItem[] items = order.getItems();
+		ArrayList<SaleItem> items = order.getItems();
 		
-		for(SaleItem item : items) {
-			subTotal += item.getPrice();
+		for(int i = 0; i < items.size(); i++) {
+			subTotal += items.get(i).getPrice();
 		}
 		
 		return subTotal;
+	}
+	
+	public static ArrayList<SaleItem> removeDuplicate(ArrayList<SaleItem> items){
+		
+		ArrayList<SaleItem> newItems = new ArrayList<SaleItem>();
+		
+		for(SaleItem item : items) {
+			
+			if(!newItems.contains(item)) {
+				newItems.add(item);
+			}
+		}
+		
+		return newItems;
 	}
 	
 	
