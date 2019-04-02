56689a4dff2c28a01a8f0285999ed30667d6f861
wongwanting <wong1089@e.ntu.edu.sg>
2019-04-02 13:25:31 +0800

    added checkId method

--- src/mgr/InvoiceManager.java
+++ src/mgr/InvoiceManager.java
@@ -1,5 +1,32 @@
 package mgr;
 
-public class InvoiceManager {
+import java.util.Scanner;
 
+//import app.RRPSSApp;
+import classes.Order;
+import classes.Restaurant;
+import classes.Invoice;
+import mgr.OrderMgr;
+
+
+
+public class InvoiceManager {
+	
+	public static boolean checkId(int orderId) {
+
+		//check 1 - if there are no orders
+		if(Restaurant.orders.size() == 0) {
+			return false;
+		}
+		
+		//check 2 - if there is no such orderId
+		for(int i = 0; i < Restaurant.orders.size(); i++) {
+			
+			if(Restaurant.orders.get(i).getId() == orderId) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
 }
