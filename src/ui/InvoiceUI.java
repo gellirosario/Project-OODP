2e0af6dfb176cb84d5b0e8381434e2100f64d5aa
wongwanting <wong1089@e.ntu.edu.sg>
2019-04-02 12:48:14 +0800

    added showInvoiceUI method

--- src/ui/InvoiceUI.java
+++ src/ui/InvoiceUI.java
@@ -1,5 +1,21 @@
 package ui;
 
-public class InvoiceUI {
+import java.util.Scanner;
 
+import mgr.InvoiceManager;
+
+
+public class InvoiceUI {
+	
+	public static Scanner sc = new Scanner(System.in);
+	
+	public static void showInvoiceUI() {
+		int orderId = 0;
+		
+		System.out.println("Please enter the order ID: ");
+		orderId = sc.nextInt();
+		
+		//call printInvoice() in InvoiceManager
+		InvoiceManager.printInvoice(orderId);
+	}
 }
